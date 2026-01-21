a424cf85813fb8b653ced023c0c19295d8c0e094
take-tomoaki-ab <take_tomoaki@applibot.co.jp>
2026-01-21 16:08:48 +0900

    can copy on side menu

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,47 @@
 // CSS selector for Wrike ticket title (to be customized)
-const WRIKE_TITLE_SELECTOR = '.title__field';
+const WRIKE_TITLE_SELECTOR = '.title__field'
+const WRIKE_MENU_DIALOG = 'WRIKE-MENU-V2'
+
+let menuKind: 'sidebar' | null = null
+let selectedMenu: string | null = null
+
+// Watch menu dialog
+const menuObserver = new MutationObserver(mutations => {
+  for (const mutation of mutations) {
+    if (mutation.type !== 'childList') return
+
+    Array.from(mutation.addedNodes).forEach(node => {
+      if ((node as Element).tagName === WRIKE_MENU_DIALOG) {
+        menuKind = 'sidebar'
+        const selectedRow = document.querySelector('.folder-tree-item--context-menu-open .tree-item-name__button-text')
+        if (selectedRow) selectedMenu = selectedRow.textContent
+      }
+    })
+
+    // Remove menu kind after copy
+    Array.from(mutation.removedNodes).forEach(node => {
+      if ((node as Element).tagName === WRIKE_MENU_DIALOG) {
+        setTimeout(() => {
+          menuKind = null
+        }, 1000);
+      }
+    })
+  }
+})
+menuObserver.observe(document.body, {
+  childList: true,
+  subtree: false
+})
 
 /**
  * Get the ticket title from the page
  * @returns The ticket title or null if not found
  */
 function getTicketTitle(): string | null {
+  if (menuKind === 'sidebar') { // menu on sidebar
+    return selectedMenu
+  }
+
   const titleElement = document.querySelector<HTMLTextAreaElement>(WRIKE_TITLE_SELECTOR);
   return titleElement ? titleElement.value : null;
 }
